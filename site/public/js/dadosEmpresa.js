547d83ebc863651db5a3f88077a4eda6ce011f67
Matheus-Ribeir <matheus.ribeiro@sptech.school>
2022-10-23 21:54:49 -0300

    Verificações parte 6

--- site/public/js/dadosEmpresa.js
+++ site/public/js/dadosEmpresa.js
@@ -216,3 +216,15 @@ else {
     limpa_formulário_cep();
 }
 };
+
+//Formatar o campo telefone, cep e cnpj
+function formatar(mascara, documento){
+  var i = documento.value.length;
+  var saida = mascara.substring(0,1);
+  var texto = mascara.substring(i)
+  
+  if (texto.substring(0,1) != saida){
+            documento.value += texto.substring(0,1);
+  }
+  
+}
